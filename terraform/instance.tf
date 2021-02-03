00b9565343157c6867f1f2cf83c3d677f4e10932
Angel Psiakis <angel@mml.rocks>
2021-02-03 20:47:17 +0000

    Create VPC, IGW, subnet, RT, etc...

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,16 +1,49 @@
 resource "aws_vpc" "remote-dev-box-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.1.0.0/16"
 
   tags = {
     Name = var.ssh_key_name
   }
 }
 
+resource "aws_internet_gateway" "remote-dev-box-igw" {
+  vpc_id = aws_vpc.remote-dev-box-vpc.id
+
+  tags = {
+    Name = var.ssh_key_name
+  }
+}
+
+resource "aws_subnet" "remote-dev-box-subnet" {
+  vpc_id = aws_vpc.remote-dev-box-vpc.id
+  cidr_block = "10.1.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = var.ssh_key_name
+  }
+}
+
+resource "aws_route_table" "remote-dev-box-subnet-rt" {
+  vpc_id = aws_vpc.remote-dev-box-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.remote-dev-box-igw.id
+  }
+
+  tags = {
+    Name = var.ssh_key_name
+  }
+}
+
+resource "aws_route_table_association" "remote-dev-box-subnet-rta" {
+  subnet_id = aws_subnet.remote-dev-box-subnet.id
+  route_table_id = aws_route_table.remote-dev-box-subnet-rt.id
+}
 
 resource "aws_security_group" "remote-dev-box-sg" {
-  name        = "remote-dev-box-sg"
   description = "Remote dev box security group"
-  vpc_id      = aws_vpc.remote-dev-box-vpc.id
+  vpc_id = aws_vpc.remote-dev-box-vpc.id
 
   dynamic "ingress" {
     for_each = var.open_ports
@@ -44,7 +77,8 @@ resource "aws_instance" "remote-dev-box" {
     volume_size = 20
   }
 
-  vpc_security_group_ids = [aws_security_group.remote-dev-box-sg.id]
+  security_groups = [aws_security_group.remote-dev-box-sg.id]
+  subnet_id = aws_subnet.remote-dev-box-subnet.id
 
     tags = {
       Name = var.ssh_key_name
